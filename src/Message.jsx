e7c98d5fdc480425edd36822f96327669a896180
mmythily <mythily.mudunuru@gmail.com>
2019-07-02 10:13:22 -0400

    changing files to play with fcc

---
+++ src/Message.jsx
@@ -0,0 +1,9 @@
+{/* <main class="messages">
+    <div class="message">
+      <span class="message-username">Anonymous1</span>
+      <span class="message-content">I won't be impressed with technology until I can download food.</span>
+    </div>
+    <div class="message system">
+      Anonymous1 changed their name to nomnom.
+    </div>
+  </main> */}
