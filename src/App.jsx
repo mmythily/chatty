8bfdeeea0db88dc222437b0ac3764ff6f344d689
mmythily <mythily.mudunuru@gmail.com>
2019-07-04 22:30:52 -0400

    incoming messages display

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,8 @@ class App extends Component {
 
     this.state = {
       currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          id:1,
-          username: 'Bob',
-          content: 'Has anyone seen my marbles?',
-        },
-        {
-          id:2,
-          username: 'Anonymous',
-          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
-        }
-      ]
+      messages: []
+      
     }
   }
   
@@ -37,29 +27,44 @@ class App extends Component {
   //display current user message on MessageList from input form
   addMessage = (content) => {
     const newMessage = {username:this.state.currentUser.name,content }
-    const messages = this.state.messages.concat(newMessage)
-    console.log(newMessage)
+    //const messages = this.state.messages.concat(newMessage)
     this.socket.send(JSON.stringify(newMessage));
-    //this.setState({messages: messages})
-    console.log('add messages', this.state)
+    //this.setState({messages:messages})
+    //console.log('add messages', this.state)
   }
 
-  //displays received messages from other users
   componentDidMount() {
+    //establish connection with websocket
     this.socket = new WebSocket('ws://localhost:3001');
-
+    
+    //display received messages from other users
     this.socket.onopen = (event) => {
       console.log('Connected to Web Socket')
       console.log('socket on open', this.state)
     }
+    //receive the broadcasted messages
     this.socket.onmessage = (event) => {
-      console.log(event)
-      
-      console.log('Message received')
+      console.log('event onmessage',event)
+      //console.log('event onmessage',event.data)
+      const receivedData = JSON.parse(event.data);
+      console.log(receivedData)
+      const postMessage = {
+        username: receivedData.username,
+        content: receivedData.content,
+        id: receivedData.id
+      }
+      const messages = this.state.messages.concat(postMessage)
+      this.setState({messages:messages})
 
-      //this.socket.send(JSON.parse(this.state.messages));
+      //console.log('display incoming messages', this.state)
+
+      console.log('Message received',postMessage)
+      //this.socket.send(postMessage);
+
+      //this.socket.close()
     }
-    
+
+
     // setTimeout(() => {
     //   const newMessage = {id: 3, username: 'Michelle', content: 'Hello there!'};
     //   const messages = this.state.messages.concat(newMessage)
@@ -67,6 +72,8 @@ class App extends Component {
     // }, 1000);
 
   }
+
+  
 
   //method that takes the content from bar
   // changeUser = (content) => {
